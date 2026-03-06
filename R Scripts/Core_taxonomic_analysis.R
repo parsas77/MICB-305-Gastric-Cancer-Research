083df1159ad541ac1d0f047a1ab90485e71bd653
Christina Xugao <chengyue005@gmail.com>
2026-03-06 15:24:35 -0800

    Reset core microbiome prevalence to 0.2 --> found more results

--- R Scripts/Core_taxonomic_analysis.R
+++ R Scripts/Core_taxonomic_analysis.R
@@ -16,16 +16,15 @@ sex.male = subset_samples(ps_rare_relab_genus, Gender == 'male')
 sex.female = subset_samples(ps_rare_relab_genus, Gender == 'female')
 
 # Find core members
-ASVs_male = core_members(sex.male, detection=0.001, prevalence = 0.8)
-ASVs_female = core_members(sex.female, detection=0.001, prevalence = 0.8)
+ASVs_male = core_members(sex.male, detection=0.001, prevalence = 0.2)
+ASVs_female = core_members(sex.female, detection=0.001, prevalence = 0.2)
 
 diagram<- ggVennDiagram(list(ASVs_male, ASVs_female),
               set_size = 5,
               category.names = c('male','female'))
+diagram
 
 ggsave("Results/Plots/core_genus_sex.png",
        diagram,
        width = 8,
        height = 5)
-# 1 core taxa unique to male and no unique core taxa for female so I 
-# don't think we need to go futher into each stages. 
