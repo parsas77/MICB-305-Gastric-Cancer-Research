592e79c646fd29ef113b4bec4e2120401a28e96e
ryann938 <ryankswong@gmail.com>
2026-03-22 16:09:35 -0700

    Updated dimensions of Faith's PD plot

--- R Scripts/alpha_diversity_analysis.R
+++ R Scripts/alpha_diversity_analysis.R
@@ -273,7 +273,7 @@ p_faith
 
 # Save Faith's PD plot
 ggsave("Results/Plots/Alpha_Faith_PD.png",
-       plot = p_faith, height = 4, width = 12)
+       plot = p_faith, height = 8, width = 5)
 
 # Save Faith's PD statistics
 write_xlsx(
