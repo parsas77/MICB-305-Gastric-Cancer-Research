c61a0dcf3be78eb12a8483038fe2b10a4b1eaac5
Christina Xugao <chengyue005@gmail.com>
2026-03-28 19:41:14 -0700

    Adjusted all core microbiome plots to be on the same scale

--- R Scripts/Core_taxonomic_analysis.R
+++ R Scripts/Core_taxonomic_analysis.R
@@ -7,8 +7,6 @@ library(patchwork)
 # Load object
 ps = readRDS('Datasets/phyloseq_taxonomy.rds')
 
-
-
 # Rarefy and convert to relative abundance and use tax_glom to the genus level
 psrare = rarefy_even_depth(ps, sample.size = 8000)
 ps_rare_relab = transform(psrare, 'compositional')
@@ -43,6 +41,33 @@ ggsave("Results/Plots/core_genus_sex.png",
 
 # Make core microbiome function ----------------
 
+get_stage_fill_max <- function(ps, detection = 0.001, prevalence = 0.2) {
+  sex.male <- subset_samples(ps, Gender == "male")
+  sex.female <- subset_samples(ps, Gender == "female")
+  
+  ASVs_male <- core_members(sex.male, detection = detection, prevalence = prevalence)
+  ASVs_female <- core_members(sex.female, detection = detection, prevalence = prevalence)
+  
+  venn_obj <- Venn(list(Female = ASVs_female, Male = ASVs_male))
+  venn_dat <- process_data(venn_obj)
+  
+  max(venn_region(venn_dat)$count, na.rm = TRUE)
+}
+
+# Find maximum value for each stage to set limit for scale fill 
+hc <- subset_samples(ps_rare_relab_genus, Group == "Healthy control (HC)")
+CG <- subset_samples(ps_rare_relab_genus, Group == "Chronic gastritis (CG)")
+IM <- subset_samples(ps_rare_relab_genus, Group == "Intestinal metaplasia (IM）")
+IN <- subset_samples(ps_rare_relab_genus, Group == "Intraepithelial neoplasia (IN)")
+GC <- subset_samples(ps_rare_relab_genus, Group == "Gastric cancer (GC)")
+
+scale_limit <- max(get_stage_fill_max(hc),
+                   get_stage_fill_max(CG), 
+                   get_stage_fill_max(IM), 
+                   get_stage_fill_max(IN), 
+                   get_stage_fill_max(GC))
+
+
 core_graph <- function(ps, detection, prevalence, title = NULL) {
   # Subset phyloseq object
   sex.male <- subset_samples(ps, Gender == "male")
@@ -58,6 +83,7 @@ core_graph <- function(ps, detection, prevalence, title = NULL) {
   ) +
     coord_cartesian(clip = "off") +
     ggtitle(title) +
+    scale_fill_continuous(limits = c(0, scale_limit))+
     theme_bw() +
     theme(
       panel.grid.major = element_blank(),
@@ -71,17 +97,17 @@ core_graph <- function(ps, detection, prevalence, title = NULL) {
 }
 
 # Run analysis by each stage -------------- 
+hc_plot <- core_graph(hc, 0.001, 0.2, "Healthy control")+
+  theme(legend.position = "none")
+hc_plot 
 
-hc <- subset_samples(ps_rare_relab_genus, Group == "Healthy control (HC)")
-hc_plot <- core_graph(hc, 0.001, 0.2, "Healthy control")
-hc_plot
 ggsave("Results/Plots/core_genus_sex_HC.png",
        hc_plot,
        width = 10,
        height = 10)
 
-CG <- subset_samples(ps_rare_relab_genus, Group == "Chronic gastritis (CG)")
-CG_plot <- core_graph(CG, 0.001, 0.2, "Chronic gastritis")
+CG_plot <- core_graph(CG, 0.001, 0.2, "Chronic gastritis")+
+  theme(legend.position = "none")
 CG_plot
 ggsave("Results/Plots/core_genus_sex_CG.png",
        CG_plot,
@@ -97,8 +123,8 @@ ggsave("Results/Plots/core_genus_sex_IM.png",
        height = 10)
 
 
-IN <- subset_samples(ps_rare_relab_genus, Group == "Intraepithelial neoplasia (IN)")
-IN_plot <- core_graph(IN, 0.001, 0.2, "Intraepithelial neoplasia")
+IN_plot <- core_graph(IN, 0.001, 0.2, "Intraepithelial neoplasia")+
+  theme(legend.position = "none")
 IN_plot
 ggsave("Results/Plots/core_genus_sex_IN.png",
        IN_plot,
@@ -106,8 +132,8 @@ ggsave("Results/Plots/core_genus_sex_IN.png",
        height = 10)
 
 
-GC <- subset_samples(ps_rare_relab_genus, Group == "Gastric cancer (GC)")
-GC_plot <- core_graph(GC, 0.001, 0.2, "Gastric cancer")
+GC_plot <- core_graph(GC, 0.001, 0.2, "Gastric cancer")+
+  theme(legend.position = "none")
 GC_plot
 ggsave("Results/Plots/core_genus_sex_GC.png",
        GC_plot,
@@ -127,3 +153,5 @@ ggsave("Results/Plots/core_genus_combined.png",
        combined_plot,
        width = 26,
        height = 18)
+
+
