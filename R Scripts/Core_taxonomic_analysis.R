55bbbf1e69e657af94e6a5279c096b830342f8c1
Christina Xugao <chengyue005@gmail.com>
2026-04-01 15:19:06 -0700

    Polish core microbiome plots

--- R Scripts/Core_taxonomic_analysis.R
+++ R Scripts/Core_taxonomic_analysis.R
@@ -12,33 +12,6 @@ psrare = rarefy_even_depth(ps, sample.size = 8000)
 ps_rare_relab = transform(psrare, 'compositional')
 ps_rare_relab_genus = tax_glom(ps_rare_relab, 'Genus')
 
-# Overall analysis - no stratification by stage ------------ 
-
-# Subset phyloseq object
-sex.male = subset_samples(ps_rare_relab_genus, Gender == 'male')
-sex.female = subset_samples(ps_rare_relab_genus, Gender == 'female')
-
-# Find core members
-ASVs_male = core_members(sex.male, detection=0.001, prevalence = 0.2)
-ASVs_female = core_members(sex.female, detection=0.001, prevalence = 0.2)
-
-diagram <- ggVennDiagram(
-  list(male = ASVs_male, female = ASVs_female),
-  set_size = 5
-) +
-  coord_cartesian(clip = "off") +
-  theme(
-    plot.margin = margin(t =10, r = 100, b = 1, l = 80)
-  )
-
-diagram
-
-ggsave("Results/Plots/core_genus_sex.png",
-       diagram,
-       width = 10,
-       height = 8)
-
-
 # Make core microbiome function ----------------
 
 get_stage_fill_max <- function(ps, detection = 0.001, prevalence = 0.2) {
@@ -79,7 +52,8 @@ core_graph <- function(ps, detection, prevalence, title = NULL) {
   
   ggVennDiagram(
     list(Female = ASVs_female, Male = ASVs_male),
-    set_size = 5
+    set_size = 8,
+    label_size = 12
   ) +
     coord_cartesian(clip = "off") +
     ggtitle(title) +
@@ -93,6 +67,7 @@ core_graph <- function(ps, detection, prevalence, title = NULL) {
       axis.title = element_blank(),
       plot.title = element_text(size = 18, hjust = 0.5, face = "bold"),
       plot.margin = margin(t =10, r = 10, b = 1, l = 10),
+      panel.border = element_blank()
     )
 }
 
@@ -101,44 +76,44 @@ hc_plot <- core_graph(hc, 0.001, 0.2, "Healthy control")+
   theme(legend.position = "none")
 hc_plot 
 
-ggsave("Results/Plots/core_genus_sex_HC.png",
+ggsave("Results/Plots/core_microbiome/core_genus_sex_HC.png",
        hc_plot,
-       width = 10,
-       height = 10)
+       width = 13,
+       height = 13)
 
 CG_plot <- core_graph(CG, 0.001, 0.2, "Chronic gastritis")+
   theme(legend.position = "none")
 CG_plot
-ggsave("Results/Plots/core_genus_sex_CG.png",
+ggsave("Results/Plots/core_microbiome/core_genus_sex_CG.png",
        CG_plot,
-       width = 10,
-       height = 10)
+       width = 13,
+       height = 13)
 
 IM <- subset_samples(ps_rare_relab_genus, Group == "Intestinal metaplasia (IM）")
 IM_plot <- core_graph(IM, 0.001, 0.2, "Intestinal metaplasia")
 IM_plot
-ggsave("Results/Plots/core_genus_sex_IM.png",
+ggsave("Results/Plots/core_microbiome/core_genus_sex_IM.png",
        IM_plot,
-       width = 10,
-       height = 10)
+       width = 13,
+       height = 13)
 
 
 IN_plot <- core_graph(IN, 0.001, 0.2, "Intraepithelial neoplasia")+
   theme(legend.position = "none")
 IN_plot
-ggsave("Results/Plots/core_genus_sex_IN.png",
+ggsave("Results/Plots/core_microbiome/core_genus_sex_IN.png",
        IN_plot,
-       width = 10,
-       height = 10)
+       width = 13,
+       height = 13)
 
 
 GC_plot <- core_graph(GC, 0.001, 0.2, "Gastric cancer")+
   theme(legend.position = "none")
 GC_plot
-ggsave("Results/Plots/core_genus_sex_GC.png",
+ggsave("Results/Plots/core_microbiome/core_genus_sex_GC.png",
        GC_plot,
-       width = 10,
-       height = 10)
+       width = 13,
+       height = 13)
 
 # Make combined graphs
 
@@ -149,7 +124,7 @@ combined_plot <- wrap_plots(
 )
 combined_plot
 
-ggsave("Results/Plots/core_genus_combined.png",
+ggsave("Results/Plots/core_microbiome/core_genus_combined.png",
        combined_plot,
        width = 26,
        height = 18)
